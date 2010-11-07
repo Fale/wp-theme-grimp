2e8de68d52811b2bee062ad8054ee42eafd33bbe
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-07 09:07:39 +0100

    Now descriptions should work properly :)

--- header.php
+++ header.php
@@ -74,7 +74,7 @@ if (top.location != self.location) top.location = self.location;
 	}?>
 	<div id="branding">
 		<h1 id="<?php if(get_option('greenpark2_logo_show')!= 'yes'){echo 'brand';}else{echo 'logo';}?>"><a href="<?php echo get_option('home'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
-		<div class="description">
+		<div class="description-<?php if(get_option('greenpark2_logo_show')!= 'yes'){echo 'brand';}else{echo 'logo';}?>">
 		  <?php bloginfo('description'); ?>
     </div>
 	</div>
