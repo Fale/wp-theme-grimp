d0d222ad6d5587cade7c2faa20d393e6ba32dbe5
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-15 15:51:19 +0100

    Remove chucks of code not used any more

--- sidebar.php
+++ sidebar.php
@@ -70,52 +70,7 @@
 	</li>
 	<?php }?>
 
-<?php endif; // end 2nd sidebar widgets  ?>
-</ul>
-
-<ul class="group">
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(3) ) : ?>
-  
-  <?php if ( is_front_page() || is_home() || is_page() ) { ?>
-    <?php wp_list_pages('title_li=<div class="sb-title">' . __('Pages','default') . '</div>' ); ?>
-  <?php } ?>
-  
-  <?php if ( is_home() || is_day() || is_month() || is_year() ) { ?>
-  	<li class="archives">
-      <div class="sb-title"><?php _e('Archives', 'default'); ?></div>
-  		<ul>
-  		  <?php wp_get_archives('type=monthly'); ?>
-  		</ul>
-  	</li>
-  <?php } ?>
-  
-  <?php if ( is_home() || is_category() ) { ?>
-    <?php wp_list_categories('show_count=1&title_li=<div class="sb-title">' . __('Categories','default') . '</div>'); ?>
-  <?php } ?>
-
-<?php endif; // end 3rd sidebar widgets  ?>
-</ul>
-
-
-<ul class="group">
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(4) ) : ?>
-
-  <?php if ( is_home() ) { ?>
-    <?php wp_list_bookmarks('title_before=<div class="sb-title">&title_after=</div>'); ?>
-  <?php } ?>
-  
-  <?php if ( is_home() || is_page() ) { ?>
-  	<li id="meta">
-      <div class="sb-title"><?php _e('Meta', 'default'); ?></div>
-    	<ul>
-    		<?php wp_register(); ?>
-    		<li><?php wp_loginout(); ?></li>
-    		<?php wp_meta(); ?>
-    	</ul>
-  	</li>
-  <?php } ?>
-
-<?php endif; // end 4th sidebar widgets  ?>
+<?php endif;?>
 </ul>
 
 </div> <!-- #sidebar -->
