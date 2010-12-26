b425c86713a3234736881d2e8966898b11a70146
Fabio Alessandro Locati <flocati@grimp.eu>
2010-12-26 11:51:22 +0100

    Better header style for personal blog

--- category-blog.php
+++ category-blog.php
@@ -5,8 +5,14 @@
 
   		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     		<div class="hentry post" id="post-<?php the_ID(); ?>">
-    		  <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e('Permanent Link to', 'default'); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-    		  <?php edit_post_link(__( 'Edit this entry', 'default' ), '<small class="meta">', '</small>'); ?>
+			    <h1><?php the_title(); ?></h1>
+			    <small class="meta">
+      			<span class="alignleft">
+              <?php the_time(__('F jS, Y', 'default')) ?> <?php _e('by', 'default'); ?> <?php the_author() ?>
+              <?php edit_post_link(__( 'Edit this entry', 'default' ), ' | ', ''); ?>
+      			</span>
+            <a href="#comments" class="alignright button-style" rel="nofollow"><?php _e('Leave a reply', 'default'); ?> &raquo;</a>
+          </small>
     			<div class="entry">
             <?php the_content(''); ?>
     			  <?php wp_link_pages(array('before' => '<div class="page-link clearfix"><strong>Pages:</strong>', 'after' => '</div>', 'next_or_number' => 'number', 'pagelink' => '<span>%</span>')); ?>
