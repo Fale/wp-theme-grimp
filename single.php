e05ce331a64c5a874e1d373f4a5ca22282702f2b
Fabio Alessandro Locati <flocati@grimp.eu>
2011-01-20 11:19:05 +0100

    Make the disclaimer ready for production

--- single.php
+++ single.php
@@ -16,14 +16,15 @@
       </small>
 
 			<div class="entry">
-<?php $c = 0;
+				<?php the_content(); ?>
+				<?php $c = 0;
 foreach (get_the_category() as $category)
-	if ($category->slug == "ciao")
+	if ($category->slug == "grimp")
 		$c = $c + 1;
 if ($c > 0)
-	echo "ATTENZIONE: Quanto scritto di seguito rappresenta solo il punto di vista dell'autore stesso e non il punto di vista dell'azienda. È possibile che quanto scritto sia addirittura contrastante con il punto di vista dell'azienda sull'argomento.";
+	echo "ATTENZIONE: Quanto scritto in questo articolo rappresenta il punto di vista dell'autore stesso e potrebbe non rappresentare il punto di vista dell'azienda. È possibile che quanto scritto sia addirittura contrastante con il punto di vista dell'azienda sull'argomento.";
  ?>
-				<?php the_content(); ?>
+
 				<?php wp_link_pages(array('before' => '<div class="page-link clearfix"><strong>Pages:</strong>', 'after' => '</div>', 'next_or_number' => 'number', 'pagelink' => '<span>%</span>')); ?>
 
       	<ul class="previousnext clearfix">
